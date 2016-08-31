157e553bdf14bc93958c6e1d355b3f20065d4533
alexberry721 <alexberry721@gmail.com>
2016-08-30 17:49:35 -0700

    Init commit

--- Book Exercise 3.4/main.cpp
+++ Book Exercise 3.4/main.cpp
@@ -5,11 +5,32 @@
 //  Created by ax on 8/30/16.
 //  Copyright © 2016 COMP130. All rights reserved.
 //
+//  Goldilocks Program
+//
 
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Goldilocks Program \n";
+    // D
+    double temp_input = 0.0;
+    
+    // I
+    cout << "Enter the temperature: ";
+    cin >> temp_input;
+    
+    // P // O
+    if (temp_input < 30) {
+        cout << "too cold" << endl;
+    } else if (temp_input > 100) {
+        cout << "too hot" << endl;
+    } else {
+        cout << "just right" << endl;
+    }
+    
+    
     return 0;
 }
